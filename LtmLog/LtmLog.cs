a0523e2949ab6377ed3cc551e5fac8a41077d299
liutm <liutm@enveesoft.com>
2014-12-08 21:10:52 +0800

    new

--- LtmLog/LtmLog.cs
+++ LtmLog/LtmLog.cs
@@ -8,6 +8,7 @@
 
 using System.IO;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace Ltm
 {
@@ -15,6 +16,7 @@ namespace Ltm
 	// 日志类型
 	public enum LOG_TYPE
 	{
+		Null = 0, 							// null
 		Unity = 1, 							// unity环境下日志类型
 		Txt, 								// 录入文件日志类型
 	}
@@ -27,86 +29,26 @@ namespace Ltm
 		Error, 								// 错误
 	}
 
-	public abstract class ILog
+	public static class LtmLog
 	{
-
-		// 当前LOG对象是否有效
-		protected bool _valid = true;
-
-		// 当前LOG对象id
-		protected string _id;
-
-		// 当前LOG对象Type
-		protected LOG_TYPE _type;
-
-		// 是否同时开启文件写入，默认关闭
-		protected bool _isFileIn = false;
-		protected FileLog _fileLog = null;
-
+	
 		// 管理所有Log对象的集合
-		protected static Dictionary<LOG_TYPE, Dictionary<string,ILog>> s_dictLogs = new Dictionary<LOG_TYPE, Dictionary<string,ILog>>();
+		private static Hashtable s_tabLogs = new Hashtable();
 
-		public string id
-		{ get{return _id;} }
-		public LOG_TYPE type
-		{get{return _type;}}
-		public bool valid
-		{get{return _valid;}}
-		public bool isFileIn
-		{get{return _isFileIn;}}
+		// 管理所有默认的LOg对象
+		private static Hashtable s_tabLogsDefault = new Hashtable();
+	
+		// 当前指定的LogType
+		private static LOG_TYPE _type = LOG_TYPE.Txt;
 
-		public ILog(LOG_TYPE typ, string id)
+		private static string GenKey( LOG_TYPE typ, string id)
 		{
-			_type = typ;
-			_id = id;
+			return typ + "__" + id;
 		}
-
-		// 设置是否同时开启文件写入
-		public void SetFileIn( bool bIn = true )
-		{
-			// 若本身就是文件日志类型，则直接返回
-			if ( this.GetType() == typeof(FileLog) )
-			{
-				_isFileIn = false;
-				return;
-			}
-
-			_isFileIn = bIn;
-
-			if ( bIn && _fileLog == null )
-			{
-				_fileLog = new FileLog(id);
-			}
-		}
-
-
-		// 设置是否有效
-		public virtual void SetValid(bool valid = true)
-		{
-			_valid = valid;
-		}
-
-
-		// 添加一个日志对象到管理集合中
-		protected static void Add(ILog log)
-		{
-			if ( log == null )
-				return;
-
-			if ( s_dictLogs.ContainsKey(log.type) == false )
-				s_dictLogs[log.type] = new Dictionary<string, ILog>();
-
-			if ( s_dictLogs[log.type].ContainsKey(log.id) == false )
-				s_dictLogs[log.type].Add( log.id, log);
-		}
-
 
 		// 获取一个类型的日志对象，如果不存在，则创建
-		public static ILog GetLog(LOG_TYPE typ, string id)
+		private static ILog GenLog(LOG_TYPE typ, string id)
 		{
-
-			if ( s_dictLogs.ContainsKey(typ) && s_dictLogs[typ].ContainsKey(id) )
-				return s_dictLogs[typ][id];
 
 			ILog log = null;
 			switch( typ )
@@ -115,7 +57,6 @@ namespace Ltm
 				{
 #if UNITY_EDITOR
 				 	log = new UnityLog(id);
-				 	Add(log);	
 #endif
 				}
 				break;
@@ -123,7 +64,6 @@ namespace Ltm
 				case LOG_TYPE.Txt:
 				{
 					log = new FileLog(id);
-					Add (log);
 				}
 				break;
 			}
@@ -131,207 +71,341 @@ namespace Ltm
 			return log;
 		}
 
-
-		// 静态的日志写入接口，返回一个日志对象
-		public static ILog WriteLog(object msg, LOG_TYPE typ, string id, LOG_LV lv = LOG_LV.Usual, bool fileIn = false )
+		// 指定一些参数
+		public static void Init( LOG_TYPE typ )
 		{
-			ILog log = GetLog( typ, id);
-			if ( log == null )
-				return null;
+			_type = typ;
+			string key = GenKey(typ,"default");
+			if ( s_tabLogsDefault.ContainsKey(key) == false )
+			 	s_tabLogsDefault[key] = GenLog(_type,"default");
 
-			log.SetValid(true);
-			log.SetFileIn(fileIn);
-
-			log.WriteLog(msg, lv);
-			return log;
 		}
 
-		// 留给外部直接调用的日志录入接口
-		public void WriteLog(object msg, LOG_LV lv = LOG_LV.Usual)
-		{
-			string msgLog = (string)msg;
 
-			switch( lv )
+		// 静态的日志写入接口
+		// fileIn 	: 0:重设为no     1:重设为yes    其他值：不变
+		public static void WriteLog(object msg, string id = null, LOG_LV lv = LOG_LV.Usual, int fileIn = 2 )
+		{
+			ILog log = null;
+			if ( id == null ) 
 			{
+				string key = GenKey( _type, "default");
+				log = (ILog)s_tabLogsDefault[key];
+			}
+			else
+			{
+				string key = GenKey( _type, id);
+				if ( s_tabLogs.ContainsKey(key) )
+					log = (ILog)s_tabLogs[key];
+				else
+				{
+					log = GenLog(_type, id);
+					s_tabLogs[key] = log;
+				}
+			}
+
+			switch( fileIn )
+			{
+				case 0:
+				{
+					log.SetFileIn(false);
+				}
+				break;
+				case 1:
+				{
+					log.SetFileIn(true);
+				}
+				break;
+	
+			}
+
+			log.WriteLogDo(msg, lv);
+			return;
+		}
+
+		private static void Turn( string id, bool bTurn )
+		{
+			string key = GenKey(_type, id);
+			if ( s_tabLogs.ContainsKey(key) && s_tabLogs[key] != null )
+				((ILog)s_tabLogs[key]).SetValid(bTurn);
+		}
+
+		private static void TurnAll(bool bTurn)
+		{
+			foreach( DictionaryEntry item in s_tabLogs )
+			{
+				if ( item.Value != null && (ILog)(item.Value) != null )
+					((ILog)(item.Value)).SetValid(bTurn);
+			}
+		}
+
+		// disable log by id
+		// if param id is not sig , the last log object will be del
+		public static void Disable( string id)
+		{
+			Turn(id,false);
+		}
+
+		public static void DisableAll()
+		{
+			TurnAll(false);
+		}
+
+		public static void Enable( string id )
+		{
+			Turn(id,true);
+
+
+		}
+
+		public static void EnableAll()
+		{
+			TurnAll(true);
+		}
+
+
+
+
+
+
+
+
+/*---------------------------------------------------------------------------------------------------
+ * -------------------------------  Base log class 		-------------------------------------------*/
+
+		private abstract class ILog
+		{
+			// 当前LOG对象id
+			public string _id;
+			
+			// 当前LOG对象Type
+			public LOG_TYPE _type;
+
+			// 是否同时开启文件写入，默认关闭
+			public bool _isFileIn = false;
+			protected FileLog _fileLog = null;
+
+			protected bool _valid = true;
+
+
+			// 是否激活
+			public virtual void SetValid(bool valid)
+			{
+				_valid = valid;
+			}
+
+
+			public ILog(LOG_TYPE typ, string id)
+			{
+				_type = typ;
+				_id = id;
+			}
+
+			// 设置是否同时开启文件写入
+			public void SetFileIn( bool bIn = true )
+			{
+				// 若本身就是文件日志类型，则直接返回
+				if ( _type == LOG_TYPE.Txt )
+				{
+					_isFileIn = false;
+					return;
+				}
+				
+				_isFileIn = bIn;
+				
+				if ( bIn && _fileLog == null )
+				{
+					_fileLog = new FileLog(_id);
+				}
+			}
+
+			// 留给manager直接调用的日志录入接口
+			public void WriteLogDo(object msg, LOG_LV lv = LOG_LV.Usual)
+			{
+				if (!_valid)
+					return;
+
+				string msgLog = (string)msg;
+				
+				switch( lv )
+				{
 				case LOG_LV.Usual:
 				{
-				 	Log(msgLog);
+					Log(msgLog);
 				}
-				break;
-
+					break;
+					
 				case LOG_LV.Warning:
 				{
-				 	LogWarning(msgLog);
+					LogWarning(msgLog);
 				}
-				break;
-
+					break;
+					
 				case LOG_LV.Error:
 				{
-				 	LogError(msgLog);
+					LogError(msgLog);
 				}
-				break;
+					break;
+				}
+				
+				
+				// 如果同时开启了文件录入，则同时写入文件
+				if ( _isFileIn && _fileLog != null )
+				{
+					_fileLog.WriteLogDo(msgLog);
+				}
 			}
 
-
-			// 如果同时开启了文件录入，则同时写入文件
-			if ( _isFileIn && _fileLog != null )
-			{
-				_fileLog.WriteLog(msgLog);
-			}
+			// 寄生类需要内部实现的录入接口
+			protected abstract void Log(object obj);
+			protected abstract void LogWarning(object obj);
+			protected abstract void LogError(object obj);
 		}
 
 
-		// 寄生类需要内部实现的录入接口
-		protected abstract void Log(object obj);
-		protected abstract void LogWarning(object obj);
-		protected abstract void LogError(object obj);
-
-	}
+/* -------------------------------  Base log class end	---------------------------------------------
+/*-------------------------------------------------------------------------------------------------*/
 
 
-	// 用于Unity编辑器下的Log类型
-	public class UnityLog : ILog
-	{
 
-		public UnityLog(string id) : base(LOG_TYPE.Unity, id)
+/*--------------------------------------------------------------------------------------------------
+ * ------------------------------- 	Unity3D type log 	--------------------------------------------*/
+		// 用于Unity编辑器下的Log类型
+		private class UnityLog : ILog
 		{
-		}
-
-		protected override void Log(object obj)
-		{
-#if UNITY_EDITOR
-			if (valid)
-				UnityEngine.Debug.Log(obj);
-#endif
-		}
-		protected override void LogWarning(object obj)
-		{
-#if UNITY_EDITOR
-			if (valid)
-				UnityEngine.Debug.LogWarning(obj);
-#endif
-		}
-		protected override void LogError(object obj)
-		{
-#if UNITY_EDITOR
-			if (valid)
-				UnityEngine.Debug.LogError(obj);
-#endif
-		}
-
-	}
-
-
-
-
-	// 写入文件LOG
-	public class FileLog : ILog
-	{
-
-		protected StreamWriter _streamWriter = null;
-		protected string _file = null;
-
-		public FileLog(string id) : base( LOG_TYPE.Txt, id)
-		{
-			_file = GenFileName(id);
-		}
-
-		~FileLog()
-		{
-			Close();
-		}
-
-		// 生成文件名，同时如果不存在路径，则生成
-		protected string GenFileName( string name, string ext = "txt")
-		{
-			string path = System.Environment.CurrentDirectory;
-			int idx = path.LastIndexOf('\\');
-			string newFile;
-			string newPath;
-			if ( idx > 0 )
-			{
-				newPath = path + "\\Log\\";
-				newFile = newPath + name + "." + ext;
-			}
-			else
-			{
-				newPath = path + "Log\\" ;
-				newFile = newPath + name + "." + ext;
-			}
-
-			if (Directory.Exists(newPath)== false)
-			{
-				Directory.CreateDirectory(newPath);
-			}
-
-			return newFile;
-		}
-
-		// 打开对应文件写入流
-		protected bool Open()
-		{
-			if ( _streamWriter != null )
-				return true;
 			
-			if ( !File.Exists(_file) )
+			public UnityLog(string id) : base(LOG_TYPE.Unity, id)
 			{
-				_streamWriter = new StreamWriter(_file,true);
 			}
-			else
+			
+			protected override void Log(object obj)
 			{
-				_streamWriter = new StreamWriter(_file, true);
+				#if UNITY_EDITOR
+				UnityEngine.Debug.Log(obj);
+				#endif
 			}
-
-			return true;
+			protected override void LogWarning(object obj)
+			{
+				#if UNITY_EDITOR
+				UnityEngine.Debug.LogWarning(obj);
+				#endif
+			}
+			protected override void LogError(object obj)
+			{
+				#if UNITY_EDITOR
+				UnityEngine.Debug.LogError(obj);
+				#endif
+			}
 		}
+/*-------------------------------	Unity3D type log end --------------------------------------------
+ * -------------------------------------------------------------------------------------------------*/
 
-		protected bool Close()
+
+
+/*--------------------------------------------------------------------------------------------------
+ * ------------------------------- 	File type log 		--------------------------------------------*/
+		// 写入文件LOG
+		private class FileLog : ILog
 		{
-			if( _streamWriter != null )
+			
+			protected StreamWriter _streamWriter = null;
+			protected string _file = null;
+			
+			public FileLog(string id) : base( LOG_TYPE.Txt, id)
 			{
-				_streamWriter.Close();
+				_file = GenFileName(id);
 			}
-	
-			_streamWriter = null;
-
-			return true;
-		}
-
-
-		protected override void Log(object obj)
-		{
-			if ( valid )
+			
+			~FileLog()
+			{
+				Close();
+			}
+			
+			// 生成文件名，同时如果不存在路径，则生成
+			protected string GenFileName( string name, string ext = "txt")
+			{
+				string path = System.Environment.CurrentDirectory;
+				int idx = path.LastIndexOf('\\');
+				string newFile;
+				string newPath;
+				if ( idx > 0 )
+				{
+					newPath = path + "\\Log\\";
+					newFile = newPath + name + "." + ext;
+				}
+				else
+				{
+					newPath = path + "Log\\" ;
+					newFile = newPath + name + "." + ext;
+				}
+				
+				if (Directory.Exists(newPath)== false)
+				{
+					Directory.CreateDirectory(newPath);
+				}
+				
+				return newFile;
+			}
+			
+			// 打开对应文件写入流
+			protected bool Open()
+			{
+				if ( _streamWriter != null )
+					return true;
+				
+				if ( !File.Exists(_file) )
+				{
+					_streamWriter = new StreamWriter(_file,true);
+				}
+				else
+				{
+					_streamWriter = new StreamWriter(_file, true);
+				}
+				
+				return true;
+			}
+			
+			protected bool Close()
+			{
+				if( _streamWriter != null )
+				{
+					_streamWriter.Close();
+				}
+				
+				_streamWriter = null;
+				
+				return true;
+			}
+			
+			
+			protected override void Log(object obj)
+			{
+				
+				Open();
+				_streamWriter.WriteLine(obj);
+				Close();
+			}
+			
+			protected override void LogWarning(object obj)
 			{
 				Open();
 				_streamWriter.WriteLine(obj);
 				Close();
 			}
-		}
-
-		protected override void LogWarning(object obj)
-		{
-			if ( valid )
+			
+			protected override void LogError(object obj)
 			{
 				Open();
 				_streamWriter.WriteLine(obj);
 				Close();
 			}
+			
+			
 		}
-
-		protected override void LogError(object obj)
-		{
-			if ( valid )
-			{
-				Open();
-				_streamWriter.WriteLine(obj);
-				Close();
-			}
-		}
+/*-------------------------------	File type log end 	--------------------------------------------
+ * -------------------------------------------------------------------------------------------------*/
 
 
 	}
-
-
-
 
 }
---
+++ LtmLog/Test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ltm;
+
+public class loca : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+		LtmLog.Init(LOG_TYPE.Unity);
+		LtmLog.WriteLog("test1", "1", LOG_LV.Usual, 1);
+		LtmLog.WriteLog("test2");
+
+		LtmLog.Disable("1");
+		LtmLog.WriteLog("test3","1");
+		LtmLog.Enable("1");
+		LtmLog.WriteLog("test4");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
